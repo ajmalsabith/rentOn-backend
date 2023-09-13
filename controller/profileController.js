3f8b76f9a4bd0074390bd3a7b25cab60f07f90bb
ajmalsabith <ajmalsabith444@gmail.com>
2023-09-13 10:58:37 +0530

    last44

--- controller/profileController.js
+++ controller/profileController.js
@@ -94,7 +94,6 @@ const editprofile = async (req, res) => {
         const userdata = await User.findOne({ _id: claims._id })
         if (userdata) {
             if (userdata.purpose=='service' ) {
-                if (userdata.qualification=='') {
                     const updatedata = await User.findOneAndUpdate({ _id: claims._id }, { $set: { name: name,place:place,phone: phone, image: req.file.filename ,aboutyou:aboutyou,qualification:qualification} })
                     if (updatedata) {
                         console.log('dududududu');
@@ -106,11 +105,8 @@ const editprofile = async (req, res) => {
                             message: 'somthing wrong...!'
                         })
                     }
-                }else{
-                    res.status(400).send({
-                        message: 'please enter your qulification'
-                    })
-                }
+                   
+                
                
             }else{
                 const updatedata = await User.findOneAndUpdate({ _id: claims._id }, { $set: { name: name,place:place,aboutyou:aboutyou, phone: phone, image: req.file.filename } })
